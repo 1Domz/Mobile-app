bd1b32dafffe58a4a0ea58285e2e5a32c398ff8e
1Domz <dominiquesimbillo03@gmail.com>
2024-08-22 16:28:08 +0800

    init

---
+++ app/(tabs)/sample.tsx
@@ -0,0 +1,8 @@
+import { ThemedText } from "@/components/ThemedText";
+
+export default function Sample() {
+    return (
+
+        <ThemedText>Kristin maligo ka naman ang asim mo na</ThemedText>
+    )
+}
